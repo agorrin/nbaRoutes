da9c64eaf1f47529bd1674733763f3ce4b5830f8
Andrew Gorringe <andrewbrian.g@gmail.com>
2015-05-19 19:45:28 -0600

    Second Update

--- js/app.js
+++ js/app.js
@@ -3,5 +3,22 @@ var app = angular.module('nbaRoutes', ['ngRoute']);
 app.config(function($routeProvider, $httpProvider){
   $httpProvider.interceptors.push('httpRequestInterceptor');
 
-  //router here
+  	$routeProvider.when('/', {
+  		templateURL: 'js/home/homeTmpl.html',
+  		controller: 'homeCtrl'
+  	});
+
+  	$routeProvider.when('/teams/:team',	{
+  		templateURL: 'js/teams/teamTmpl.html',
+  		controller: 'teamCtrl', 
+      resolve:  {
+        teamData: function($route, teamService)  {
+           return teamService.getTeamData($routeParams.current.params.team);
+        }.otherwise ({
+          redirectTo: '/'
+        })
+      }
+  	});
+  	
+
 });
--- js/teams/teamService.js
+++ js/teams/teamService.js
@@ -2,14 +2,14 @@ var app = angular.module('nbaRoutes');
 
 app.service('teamService', function($http, $q){
 
-	$scope.addNewGame = function(gameObj)	{
+	this.addNewGame = function(gameObj)	{
 		var url = "https://api.parse.com/1/classes/" + gameObj.homeTeam;
 		if (parseInt(gameObj.homeTeamScore) > parseInt(gameObj.opponentScore))	{
-			Object.defineProperty(gameObj, 'won'	{
+			Object.defineProperty(gameObj, 'won',	{
 				value: true
 			});
 		} else 	{
-			Object.defineProperty(gameObj, 'won'	{
+			Object.defineProperty(gameObj, 'won',	{
 				value: false
 			});
 		}
@@ -21,7 +21,7 @@ app.service('teamService', function($http, $q){
 		})
 	}
 
-	$scope.getTeamData = function(team)	{
+	this.getTeamData = function(team)	{
 
 		var deferred = $q.defer();
 		var url = 'https://api.parse.com/1/classes/' + team;
@@ -35,13 +35,13 @@ app.service('teamService', function($http, $q){
 			for (var i = 0; i < results.length; i++) {
 				if(results[i].won === true){
 					wins++
-					Object.defineProperty(results, 'wins'	{
-						value: wins;
+					Object.defineProperty(results, 'wins',	{
+						value: wins
 					});
 				} else	{
 					losses++
-					Object.defineProperty(results, 'losses'	{
-						value: losses;
+					Object.defineProperty(results, 'losses',	{
+						value: losses
 					});
 				}
 			};
